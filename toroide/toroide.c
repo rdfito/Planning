b352004b49aab3b3282de9c3d9d87d85985d4167
Rafael D. Fito <fitooo@gmail.com>
2013-11-05 19:19:56 +0100

    c002

--- toroide/toroide.c
+++ toroide/toroide.c
@@ -2,71 +2,77 @@
 #include <string.h>
 #include <mpi.h>
 
-#define BUFFSIZE 128
 #define TAG 0
+#define CTRL 0
+#define DATA 1
 #define N 0
 #define E 1 
 #define S 2
 #define W 3
 
-int vecino(int dir, int lado, int id,);
+int vecino(int dir, int lado, int id);
 
 int main(int argc, char *argv[]) {
-	int idr, numr;  /* rank y size */
-	char buff[BUFFSIZE];
+	int rank, size;
+    int valor;
     MPI_Status st;
 
     MPI_Init(&argc, &argv);
-	MPI_Comm_size(MPI_COMM_WORLD, &numr);
-    MPI_Comm_rank(MPI_COMM_WORLD, &idr);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     
-    if(idr == 0) {
-	    int datos[numr];
+    if(rank == 0) {
+        int datos[size];
         int i = 0;
+        int parada = 0;
+        int n;
         FILE *fp; 
         
-        printf("#%d: Red toroidal de %d procesadores.\n", idr, numr);
+        printf("#%d: Red toroidal de %d procesadores.\n", rank, size);
         fp = fopen("datos.dat", "r");
         if(fp == NULL) {
             printf("Error: Archivo de datos no encontrado\n");
         } else {
-            int c;
             while(!feof(fp)) {
-                if(fscanf(fp, "%d%*c", &c) == 1) {
-                    if(i <= numr-1) {
-                        datos[i] = c;
+                if(fscanf(fp, "%d%*c", &n) == 1) {
+                    if(i <= size-1) {
+                        datos[i] = n;
                     }
                     i++;
                 }
             }
             fclose(fp);
-            if(i != numr) {
-                printf("Error: numero de datos incorrecto.\n");
-                printf("  Procs:%d Datos:%d\n", numr, i);
-                i=0;
-                while(i<numr) {
-                    MPI_Send(1, 1, MPI_INT, i, TAG, MPI_COMM_WORLD);
-                }
+
+            if(i != size) {
+                printf("Error: numero de datos incorrecto (%d para %d procs)\n", i, size);
+                parada = 1;
             }
-            else {
+            i=0;
+            while(i < size) {
+                MPI_Send(&parada, 1, MPI_INT, i, CTRL, MPI_COMM_WORLD);
+                i++;
+            }
+            if(parada == 0) {
                 i=0;
-                while(i<numr) {
-                    MPI_Send(0, 1, MPI_INT, i, TAG, MPI_COMM_WORLD);
-                    sprintf(buff, "n:%d", datos[i]);
-                    MPI_Send(buff, BUFFSIZE, MPI_CHAR, i, TAG, MPI_COMM_WORLD);
+                while(i < size) {
+                    MPI_Send(&datos[i], 1, MPI_INT, i, DATA, MPI_COMM_WORLD);
                     i++;
                 }
             }
         }
     }
-    MPI_Recv(buff, BUFFSIZE, MPI_CHAR, 0, TAG, MPI_COMM_WORLD, &st);
-    printf("#%d: %s\n", idr, buff);
+    
+    MPI_Recv(&valor, 1, MPI_INT, 0, CTRL, MPI_COMM_WORLD, &st);
+    if(valor == 0) {
+        MPI_Recv(&valor, 1, MPI_INT, 0, DATA, MPI_COMM_WORLD, &st);
+        printf("#%d: Dato=%d.\n", rank, valor);
+    }
 
-    for(i=1;i<lado;i++) {
+    /*for(i=1;i<lado;i++) {
         Send(vecino(N));
         Recv(vecino(S));
         comparar;
-    }
+    }*/
 
     MPI_Finalize();
 	
