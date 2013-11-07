dc823b394853a8e45d038c2791b008a8972d4036
Rafael D. Fito <fitooo@gmail.com>
2013-11-07 13:17:34 +0100

    c003

--- toroide/toroide.c
+++ toroide/toroide.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <mpi.h>
 
+#define DOTDAT "datos.dat"
 #define TAG 0
 #define CTRL 0
 #define DATA 1
@@ -14,27 +15,33 @@ int vecino(int dir, int lado, int id);
 
 int main(int argc, char *argv[]) {
 	int rank, size;
-    int valor;
+    double valor;
+    int parada;
     MPI_Status st;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     
+    /* RANK 0 */
     if(rank == 0) {
-        int datos[size];
+        double datos[size];
+        double n;
         int i = 0;
-        int parada = 0;
-        int n;
         FILE *fp; 
-        
-        printf("#%d: Red toroidal de %d procesadores.\n", rank, size);
-        fp = fopen("datos.dat", "r");
+        parada = 0;
+
+        printf("[%d] Red toroidal de %d procesos\n", rank, size);
+
+        /* Verificacion del archivo de datos */
+        fp = fopen(DOTDAT, "r");
         if(fp == NULL) {
-            printf("Error: Archivo de datos no encontrado\n");
+            printf("Error: Archivo \"%s\" no encontrado\n", DOTDAT);
+            parada = 1;
         } else {
             while(!feof(fp)) {
-                if(fscanf(fp, "%d%*c", &n) == 1) {
+                /* lee double seguido de char (que ignora) */
+                if(fscanf(fp, "%lf%*c", &n) == 1) {
                     if(i <= size-1) {
                         datos[i] = n;
                     }
@@ -42,30 +49,32 @@ int main(int argc, char *argv[]) {
                 }
             }
             fclose(fp);
-
-            if(i != size) {
-                printf("Error: numero de datos incorrecto (%d para %d procs)\n", i, size);
-                parada = 1;
-            }
+        }
+        /* Verificacion de valores del archivo de datos */
+        if(i != size) {
+            printf("Error: %d valores para %d procesos\n", i, size);
+            parada = 1;
+        }
+        /* Ennvio de control */
+        i=0;
+        while(i < size) {
+            MPI_Send(&parada, 1, MPI_INT, i, CTRL, MPI_COMM_WORLD);
+            i++;
+        }
+        /* Envio de datos */
+        if(parada == 0) {
             i=0;
             while(i < size) {
-                MPI_Send(&parada, 1, MPI_INT, i, CTRL, MPI_COMM_WORLD);
+                MPI_Send(&datos[i], 1, MPI_DOUBLE, i, DATA, MPI_COMM_WORLD);
                 i++;
-            }
-            if(parada == 0) {
-                i=0;
-                while(i < size) {
-                    MPI_Send(&datos[i], 1, MPI_INT, i, DATA, MPI_COMM_WORLD);
-                    i++;
-                }
             }
         }
     }
     
-    MPI_Recv(&valor, 1, MPI_INT, 0, CTRL, MPI_COMM_WORLD, &st);
-    if(valor == 0) {
-        MPI_Recv(&valor, 1, MPI_INT, 0, DATA, MPI_COMM_WORLD, &st);
-        printf("#%d: Dato=%d.\n", rank, valor);
+    MPI_Recv(&parada, 1, MPI_INT, 0, CTRL, MPI_COMM_WORLD, &st);
+    if(parada == 0) {
+        MPI_Recv(&valor, 1, MPI_DOUBLE, 0, DATA, MPI_COMM_WORLD, &st);
+        printf("[%d] Valor = %.3f\n", rank, valor);
     }
 
     /*for(i=1;i<lado;i++) {
